94bf0597b147c68f084545d37cdbb56e59307bcc
ansriaz67 <118793451+ansriaz67@users.noreply.github.com>
2022-11-22 11:00:57 +0500

    Initial Commit

---
+++ BulkyBookWeb/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LoginController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(LoginModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = _db.RegisterModel.Where(e=>e.Email== obj.Email).SingleOrDefault();
+                if(data != null)
+                {
+                    bool isValid = (data.Email == obj.Email && data.Password == obj.Password);
+                    if(isValid)
+                    {
+                        return RedirectToAction("Index","Home");
+                    }
+                    else
+                    {
+                        TempData["error"] = "You have entered the wrong password.";
+                        return View();
+                    }
+                }
+                else
+                {
+                    TempData["error"] = "Something went wrong.";
+                    return View();
+                }
+
+            }
+            TempData["error"] = "Something went wrong.";
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.RegisterModel.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "User Registered Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ BulkyBookWeb/Models/RegisterModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class RegisterModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation password not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
