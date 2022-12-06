81a13639682474acbe2113dfca3e3273fe74bb8f
ansriaz67 <118793451+ansriaz67@users.noreply.github.com>
2022-12-06 10:28:01 +0500

    Frontend Completed

---
+++ BulkyBookWeb/Models/ServiceResponce.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class ServiceResponce
+    {
+        public string Data { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = null;
+    }
+}
